766e9b2368b4d627032dc32de2fb6c90d98f8137
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-10-29 15:29:54 +0200

    fixes description

--- js/script.js
+++ js/script.js
@@ -6,32 +6,31 @@ window.addEventListener("DOMContentLoaded", function () {
     const vueApp = new Vue({
         el: "#root",
         data: {
-            message: "ciao mondo",
             imageAll: [
                 {
                     url: 'img/01.jpg',
                     title: 'Svezia',
-                    descriptio: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+                    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
                 },
                 {
                     url: 'img/02.jpg',
                     title: 'Svizzera',
-                    descriptio: 'Lorem ipsum'
+                    description: 'Lorem ipsum'
                 },
                 {
                     url: 'img/03.jpg',
                     title: 'Gran Bretagna',
-                    descriptio: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+                    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
                 },
                 {
                     url: 'img/04.jpg',
                     title: 'Germania',
-                    descriptio: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+                    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
                 },
                 {
                     url: 'img/05.jpg',
                     title: 'Paradise',
-                    descriptio: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+                    description: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
                 }
             ]
         },
