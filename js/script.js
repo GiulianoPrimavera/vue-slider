91a8901fb55841858d8dd0e7ff8f12ab2706a857
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-10-29 15:06:12 +0200

    adds an object with the information of every image

--- js/script.js
+++ js/script.js
@@ -1,12 +1,39 @@
 
-window.addEventListener("DOMContentLoaded", function (){
+window.addEventListener("DOMContentLoaded", function () {
 
     Vue.config.devtools = true;
 
-    const vueApp = new Vue ({
-        el : "#root",
+    const vueApp = new Vue({
+        el: "#root",
         data: {
-            message: "ciao mondo"
+            message: "ciao mondo",
+            imageAll: [
+                {
+                    url: 'img/01.jpg',
+                    title: 'Svezia',
+                    descriptio: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+                },
+                {
+                    url: 'img/02.jpg',
+                    title: 'Svizzera',
+                    descriptio: 'Lorem ipsum'
+                },
+                {
+                    url: 'img/03.jpg',
+                    title: 'Gran Bretagna',
+                    descriptio: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+                },
+                {
+                    url: 'img/04.jpg',
+                    title: 'Germania',
+                    descriptio: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+                },
+                {
+                    url: 'img/05.jpg',
+                    title: 'Paradise',
+                    descriptio: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+                }
+            ]
         },
         methods: {
 
@@ -14,3 +41,5 @@ window.addEventListener("DOMContentLoaded", function (){
     })
 
 })
+
+
